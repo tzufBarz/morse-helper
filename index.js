771c0be46a2eaa51e3bb018016b0688c770f99d4
tzufBarz <81tzufb5@dalet.school>
2025-01-04 14:31:28 +0200

    Added word generating functionality and design

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 let letters = {};
+let words = [];
+const outputElement = document.getElementById("output");
+
+const wordcountInput = document.getElementById("wordcount-input")
+
+fetch("words.json")
+    .then(response => response.json())
+    .then(data => {
+        words = data;
+    })
+    .catch(error => {
+        console.error('Error loading the JSON file:', error);
+    });
 
 function updateCookie() {
     document.cookie = `letters=${JSON.stringify(letters)}; path=/; max-age=${60 * 60 * 24 * 7}; Samesite=Lax`;
@@ -37,7 +50,25 @@ for (letter in letters) {
 
 window.addEventListener("beforeunload", updateCookie)
 
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) { 
+    
+        // Generate random index 
+        const j = Math.floor(Math.random() * (i + 1));
+                      
+        // Swap elements at indices i and j
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
 
 function chipClick(letter, state) {
     letters[letter] = state;
+}
+
+function generate() {
+    let filteredWords = words.filter(word => [...word].every(char => letters[char]));
+    shuffle(filteredWords);
+    outputElement.innerText = filteredWords.slice(0, parseInt(wordcountInput.value, 10)).join(" ");
 }
